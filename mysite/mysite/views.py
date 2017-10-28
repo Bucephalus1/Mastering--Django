1d4e6dbb1453d65aa34e6f80c7263d3cc16a7a9a
Bucephalus1 <jmbullen21@gmail.com>
2017-10-28 15:14:30 -0400

    Chapter 3 - Template Inheritance

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,4 @@
-from django.http import HttpResponse
-from django.template.loader import get_template
-from django.template import Context
+from django.shortcuts import render
 import datetime
 
 def hello(request):
@@ -8,9 +6,7 @@ def hello(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    t = get_template('current_datetime.html')
-    html = t.render(Context({'current_date': now }))
-    return HttpResponse(html)
+    return render(request,'current_datetime.html',{'current_date':now})
 
 def hours_ahead(request,offset):
     try:
@@ -18,5 +14,4 @@ def hours_ahead(request,offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = 'In {!s} hour(s), it will be {!s}.'.format(offset,dt)
-    return HttpResponse(html)
+    return render(request,'hours_ahead.html',{'hour_offset':offset,'next_time':dt})
